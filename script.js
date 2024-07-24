c4e69c3872f16f35cce0811068495eff384cd4eb
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 01:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const priceElements = document.querySelectorAll(".price");
+    let totalPrice = 0;
+	priceElements.forEach(priceElement => {
+        totalPrice += parseFloat(priceElement.textContent);
+    });
+	const totalRow = document.createElement("tr");
+	const totalCell = document.createElement("td");
+    totalCell.setAttribute("colspan", "2");
+    totalCell.style.fontWeight = "bold";
+    totalCell.textContent = "Total Price: Rs " + totalPrice;
+	totalRow.appendChild(totalCell);
+	const table = document.querySelector("table");
+    table.appendChild(totalRow);
   
 };
 
